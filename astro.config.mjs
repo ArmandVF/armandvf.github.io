70538e8f214212c207708d424f0dd989a7aad0d9
Armand <162913205+ArmandVF@users.noreply.github.com>
2024-03-15 01:27:52 +0000

    Update astro.config.mjs
    Fix unnecessary base for publishing to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,4 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://armandvf.github.io',
-    base: '/users-armand-github-armandvf-github-io',
 });
