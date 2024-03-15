c8fc05498db01950abae96da278e8a176221ecb1
Armand <162913205+ArmandVF@users.noreply.github.com>
2024-03-15 02:42:00 +0000

    Icon fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://armandvf.github.io',
+  site: 'https://armandvf.github.io',
+  integrations: [icon()]
 });
