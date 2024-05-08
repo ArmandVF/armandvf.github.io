4d54095a605f5b49a12bee2e95ee8965f713de7b
Armand <162913205+ArmandVF@users.noreply.github.com>
2024-05-08 16:39:31 +0100

    Redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({site: 'https://armandvf.github.io',
-  redirects: [{'/' : 'https://youtu.be/x43L8cieuXw?si=UJyLhjizqbW8g0nC'}],
   integrations: [icon()]
 })
